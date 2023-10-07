1045e55bd6cd2a3ff22c81de420b194567a445c5
Barkha Wadhvani <barkha.wadhvani@gmail.com>
2023-10-07 21:58:23 +0530

    Add files via upload

---
+++ Data Science .py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Introduction
+
+# ### Data science is a multidisciplinary field that involves the use of various techniques, algorithms, processes, and systems to extract valuable insights and knowledge from data. It combines elements from computer science, statistics, mathematics, and domain-specific expertise to analyze and interpret data in order to make informed decisions and solve complex problems.
+
+# # Data Science Languages
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. SAS
+# 6. Java
+# 7. Scala
+# 8. Matlab
+# 9. Ruby
+
+# # Data Science Libraries
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+# 1. Seaborn
+# 1. Scikit-Learn
+# 1. TensorFlow
+# 1. PyTorch
+# 1. Keras
+# 1. Statsmodels
+# 1. NLTK
+# 1. Beautiful Soup
+
+# | Tool              | Description                                                     |
+# | ----------------- | --------------------------------------------------------------- |
+# | Python            | A versatile programming language with numerous data science libraries. |
+# | R                 | A statistical programming language with a strong focus on data analysis. |
+# | SQL               | A domain-specific language for managing and querying relational databases. |
+# | Jupyter Notebook  | An interactive environment for data analysis and visualization. |
+# | Pandas            | A powerful data manipulation and analysis library for Python.  |
+# | NumPy             | A fundamental package for numerical computations in Python.     |
+# | Matplotlib        | A popular Python library for creating static, animated, or interactive plots. |
+# | Scikit-Learn      | A machine learning library with tools for classification, regression, and more. |
+# | TensorFlow        | An open-source machine learning framework developed by Google.  |
+# | PyTorch           | An open-source deep learning framework developed by Facebook.   |
+# 
+
+# 
+# # Introduction to Arithmetic Expressions
+# 
+# 
+# Arithmetic expressions involve mathematical operations such as addition, subtraction, multiplication, and division. In Python, you can perform these operations using operators like +, -, *, and /.
+# 
+# 
+
+# # Add an Multiply numbers
+
+# In[6]:
+
+
+a=5
+b=10
+print("Addition of Two numbers",a+b)
+print("Multiplication of two numbers",a*b)
+
+
+# # Convert Minutes to  Hour
+
+# In[7]:
+
+
+# Input: Number of minutes
+minutes = 120  # Replace this with the number of minutes you want to convert
+
+# Convert minutes to hours
+hours = minutes / 60
+
+# Output the result
+print(f"{minutes} minutes is equal to {hours} hours.")
+
+
+# # Unordered List
+# ## Data Science Objectives
+# - Collect and clean data
+# - Perform exploratory data analysis (EDA)
+# - Build predictive models
+# - Conduct statistical analysis
+# - Create data visualizations
+# - Deploy machine learning models
+# - Extract actionable insights
+# - Optimize decision-making processes
+# - Improve business operations
+# - Ensure data privacy and security
+
+# # Author details
+# ## Barkha Wadhvani
+# ### Area of Interest 
+# - Machine Learning
+# - Cyber Security
+
+# In[ ]:
+
+
+
+
